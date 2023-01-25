1f4a081894d63735fa0ae19e650365a8df6fde31
Ry <OfficiallyRy@outlook.com>
2023-01-24 22:39:14 -0700

    Finished clues 1 2 and 3 by myself

--- app/clue2.js
+++ app/clue2.js
@@ -6,6 +6,11 @@
 
 function largerNum(num1, num2) {
   // TODO YOUR CODE HERE
+  if(num1 > num2){
+    return num1
+  }else{
+    return num2
+  }
 }
 
 
@@ -21,6 +26,18 @@ function largerNum(num1, num2) {
 
 function crimeGrader(successful, total) {
   // TODO YOUR CODE HERE
+  average = successful / total * 100
+  if(average <= 59){
+    return "F"
+  }else if(average <= 69){
+    return "D"
+  }else if(average <= 79){
+    return "C"
+  }else if(average <= 89){
+    return "B"
+  }else if(average <= 100){
+    return "A"
+  }
 }
 
 
@@ -33,6 +50,15 @@ function crimeGrader(successful, total) {
 //   Make sure your ranges are inclusive
 function timeOfDay(hour) {
   // TODO YOUR CODE HERE
+  if(hour <= 4 || hour >= 22){
+    return "night"
+  }else if(hour <= 11){
+    return "morning"
+  }else if(hour <= 17){
+    return "afternoon"
+  }else if(hour <= 21){
+    return "evening"
+  }
 }
 
 // Our surveillance team finds the closer we get to catching the associate the hotter the person gets, we can use this to narrow down the person
@@ -40,6 +66,13 @@ function timeOfDay(hour) {
 // Write a function that will take in a number and return 'suspicious' if it indicates the person is over 98.6° and if the person is at or above 103° 'very suspicious', if it is under return 'not suspicious', (hint: try this with string concatenation)
 function isSuspicious(temp) {
   // TODO YOUR CODE HERE
+  if(temp <= 98.6){
+    return "not suspicious"
+  }else if(temp < 103){
+    return "suspicious"
+  }else{
+    return "very suspicious"
+  }
 }
 
 // We think we might have found the associate, and have added a tracker to their car, to find where the associate is hiding we need to know when their car is stopped
@@ -56,6 +89,11 @@ let exampleCar = {
 
 function isStopped(car) {
   // TODO YOUR CODE HERE  
+  if(car.moving == false){
+    return true
+  }else{
+    return false
+  }
 }
 
 // We have found everything we need to capture the associate, to make sure they don't elude us we want to make sure we only capture them when they are at home and their asleep.
@@ -70,4 +108,9 @@ let suspect = {
 
 function attemptCapture(suspect) {
   // TODO YOUR CODE HERE
+  if(suspect.atHome == true && suspect.asleep == true){
+    return true
+  }else{
+    return false
+  }
 }
